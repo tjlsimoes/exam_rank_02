7b3888411b3d866399e95a48066e47d571886e6d
tjorge-l <tjorge-l@student.42lisboa.com>
2024-05-27 13:32:35 +0100

    Add malfunctioning solution

--- level_3/add_prime_sum/add_prime_sum.c
+++ level_3/add_prime_sum/add_prime_sum.c
@@ -22,6 +22,8 @@ int	ft_atoi(char *s)
 	i = 0;
 	units = 1;
 	sign = 1;
+	if (!s)
+		return (-444);
 	if (s[i] == '-' || s[i] == '+')
 	{
 		if (s[i] == '-')
@@ -32,8 +34,32 @@ int	ft_atoi(char *s)
 	{
 		nbr = units * nbr + (s[i] + 48); 
 		units *= 10;
+		i++;
 	}
 	return (nbr * sign);
+}
+
+void	ft_putnbr(int nbr)
+{
+	long nbr_long;
+	char c;
+
+	nbr_long = (long) nbr;
+	if (nbr < 0)
+	{
+		write(1, "-", 1);
+		nbr_long *= -1;
+	}
+	if (nbr_long < 9)
+	{
+		c = nbr_long - 48;
+		write(1, &c, 1);
+	}
+	else
+	{
+		ft_putnbr(nbr_long / 10);
+		ft_putnbr(nbr_long % 10);
+	}
 }
 
 int	is_prime(int nbr)
@@ -53,28 +79,40 @@ int	is_prime(int nbr)
 			flag = 0;
 		i++;
 	}
-	return (!flag);
+	if (flag)
+		return (0);
+	return (1);
 }
 
 // https://www.simplilearn.com/tutorials/c-tutorial/c-program-for-prime-numbers
 // https://www.geeksforgeeks.org/c-program-to-check-prime-number-by-creating-a-function/
 
+void error(void)
+{
+	ft_putchar('0');
+	write(1, "\n", 1);
+}
+
 int	main(int argc, char **argv)
 {
 	int	i;
 	int	nbr;
+	int	sum;
 
 	i = 0;
+	sum = 0;
 	nbr = ft_atoi(argv[1]);
-	if (argc != 2 || nbr <= 0)
+	if (argc != 2 || nbr == -444)
 	{
-		ft_putchar('0');
-		write(1, "\n", 1);
+		error();
 		return (1);
 	}
 	while(i <= nbr)
 	{
-		if (i % 2)
+		if (is_prime(i))
+			sum += i;
+		i++;
 	}
+	ft_putnbr(sum);
 	return (0);
 }
