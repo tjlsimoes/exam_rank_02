0acdcc4e5ed4f10763ea7c71e9dc9db834d45e05
tjlsimoes <118819479+tjlsimoes@users.noreply.github.com>
2024-05-29 14:27:11 +0100

    Sketch complete solution to flood_fill

--- level_4/flood_fill/flood_fill.c
+++ level_4/flood_fill/flood_fill.c
@@ -25,25 +25,41 @@ int		get_actual_begin_idx(t_point begin)
 	return (i);
 }
 
+void	define_temp(int x, int y, t_point *temp)
+{
+	(*temp).x = x;
+	(*temp).y = y;
+}
+
 void  derived_flood_fill(char **tab, t_point size, t_point begin,
 							char replace_char)
 {
 	t_point	orig = begin;
+	t_point temp;
 
 	tab[size.y - begin.y][begin.x * 2 - 2] = 'F';
 	// Horizontal Fill
+	begin.x++;
 	while (begin.x * 2 - 2 <= size.x * 2 - 2)
 	{
 		if (tab[size.y - begin.y][begin.x * 2 - 2] == replace_char)
-			tab[size.y - begin.y][begin.x * 2 - 2] = 'F';
+		{
+			define_temp(begin.x, begin.y, &temp);
+			derived_flood_fill(tab, size, temp, replace_char);
+		}
 		else
 			break;
 		begin.x++;
 	}
+	begin.x = orig.x;
+	begin.x--;
 	while (begin.x * 2 - 2 >= 0)
 	{
 		if (tab[size.y - begin.y][begin.x * 2 - 2] == replace_char)
-			tab[size.y - begin.y][begin.x * 2 - 2] = 'F';
+		{
+			define_temp(begin.x, begin.y, &temp);
+			derived_flood_fill(tab, size, temp, replace_char);
+		}
 		else
 			break;
 		begin.x--;
@@ -51,30 +67,33 @@ void  derived_flood_fill(char **tab, t_point size, t_point begin,
 
 	// Vertical Fill
 	// Downwards
+	begin.y--;
 	while (size.y - begin.y <= size.y - 1)
 	{
 		if (tab[size.y - begin.y][orig.x * 2 - 2] == replace_char)
-			tab[size.y - begin.y][orig.x * 2 - 2] = 'F';
+		{
+			define_temp(orig.x, begin.y, &temp);
+			derived_flood_fill(tab, size, temp, replace_char);
+		}
 		else
 			break;
 		begin.y--;
 	}
 	// Upwards
+	begin.y = orig.y;
+	begin.y++;
 	while (size.y - begin.y >= 0)
 	{
 		if (tab[size.y - begin.y][orig.x * 2 - 2] == replace_char)
-			tab[size.y - begin.y][orig.x * 2 - 2] = 'F';
+		{
+			define_temp(orig.x, begin.y, &temp);
+			derived_flood_fill(tab, size, temp, replace_char);
+		}
 		else
 			break;
 		begin.y++;
 	}
 
-}
-
-t_point	define_temp(int x, int y, t_point *temp)
-{
-	(*temp).x = x;
-	(*temp).y = y;
 }
 
 void  flood_fill(char **tab, t_point size, t_point begin)
@@ -85,42 +104,60 @@ void  flood_fill(char **tab, t_point size, t_point begin)
 
 	tab[size.y - begin.y][begin.x * 2 - 2] = 'F';
 	// Horizontal Fill
+	begin.x++;
 	while (begin.x * 2 - 2 <= size.x * 2 - 2)
 	{
 		if (tab[size.y - begin.y][begin.x * 2 - 2] == replace_char)
-		// {
-		// 	define_temp(begin.x, size.y, &temp);
-		// 	derived_flood_fill(tab, size, temp, replace_char);
-		// }
-		tab[size.y - begin.y][begin.x * 2 - 2] = 'F';
+		{
+			define_temp(begin.x, begin.y, &temp);
+			derived_flood_fill(tab, size, temp, replace_char);
+		}
 		else
 			break;
 		begin.x++;
 	}
+	begin.x = orig.x;
+	begin.x--;
 	while (begin.x * 2 - 2 >= 0)
 	{
 		if (tab[size.y - begin.y][begin.x * 2 - 2] == replace_char)
-			tab[size.y - begin.y][begin.x * 2 - 2] = 'F';
+		{
+			define_temp(begin.x, begin.y, &temp);
+			derived_flood_fill(tab, size, temp, replace_char);
+		}
 		else
 			break;
 		begin.x--;
 	}
 
+	// begin.x will perhaps have been altered. Thus use of orig.x.
+	// Possible refactor: simply reset begin.x with orig.x and
+	// use begin.x downbelow.
+
 	// Vertical Fill
 	// Downwards
+	begin.y--;
 	while (size.y - begin.y <= size.y - 1)
 	{
 		if (tab[size.y - begin.y][orig.x * 2 - 2] == replace_char)
-			tab[size.y - begin.y][orig.x * 2 - 2] = 'F';
+		{
+			define_temp(orig.x, begin.y, &temp);
+			derived_flood_fill(tab, size, temp, replace_char);
+		}
 		else
 			break;
 		begin.y--;
 	}
 	// Upwards
+	begin.y = orig.y;
+	begin.y++;
 	while (size.y - begin.y >= 0)
 	{
 		if (tab[size.y - begin.y][orig.x * 2 - 2] == replace_char)
-			tab[size.y - begin.y][orig.x * 2 - 2] = 'F';
+		{
+			define_temp(orig.x, begin.y, &temp);
+			derived_flood_fill(tab, size, temp, replace_char);
+		}
 		else
 			break;
 		begin.y++;
@@ -128,19 +165,69 @@ void  flood_fill(char **tab, t_point size, t_point begin)
 
 }
 
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	if (!str)
+		return (0);
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char *ft_strdup(char *str)
+{
+	char *new_str;
+	int	i;
+
+	if (!str)
+		return (NULL);
+	i = 0;
+	new_str = (char *)malloc(ft_strlen(str) + 1);
+	if (!new_str)
+		return(NULL);
+	while (str[i])
+	{
+		new_str[i] = str[i];
+		i++;
+	}
+	new_str[i] = '\0';
+	return (new_str);
+}
+
+
 int	main(void)
 {
-	char *zone[] = {
-		"1 1 1 1 1 1 1 1",
-		"1 0 0 0 1 0 0 1",
-		"1 0 0 1 0 0 0 1",
-		"1 0 1 1 0 0 0 1",
-		"1 1 1 0 0 0 0 1",
-	};
-	// t_point size = {8, 5};
-	// t_point begin = {2, 2};
+	char **area;
+	// char *zone[] = {
+	// 	"1 1 1 1 1 1 1 1",
+	// 	"1 0 0 0 1 0 0 1",
+	// 	"1 0 0 1 0 0 0 1",
+	// 	"1 0 1 1 0 0 0 1",
+	// 	"1 1 1 0 0 0 0 1",
+	// };
+	int n = 4;
+	int	i = 0;
+	area = (char **)malloc(5 * sizeof(char *));
+	area[0] = ft_strdup("1 1 1 1 1 1 1 1");
+	area[1] = ft_strdup("1 0 0 0 1 0 0 1");
+	area[2] = ft_strdup("1 0 0 1 0 0 0 1");
+	area[3] = ft_strdup("1 0 1 1 0 0 0 1");
+	area[4] = ft_strdup("1 1 1 0 0 0 0 1");
+	t_point size = {8, 5};
+	t_point begin = {2, 2};
 	// printf("%d\n", get_actual_begin_idx(begin));
 	// printf("%c\n", get_begin_char(zone, size, begin));
-	printf("%c\n", zone[2][18]);
+	// printf("%c\n", zone[2][18]);
+	flood_fill(area, size, begin);
+
+	while (i <= 4)
+		printf("%s\n", area[i++]);
+
+	while (n >= 0)
+		free(area[n--]);
+	free(area);
 	return (0);
 }
--- level_4/flood_fill/flood_fill.h
+++ level_4/flood_fill/flood_fill.h
@@ -1,6 +1,8 @@
 #ifndef FLOOD_FILL_H
 # define FLOOD_FILL_H
 #include <stdio.h>
+#include <stdlib.h>
+
 typedef struct  s_point
 {
 int           x;
@@ -8,6 +10,9 @@ int           y;
 }               t_point;
 
 char	get_begin_char(char **tab, t_point size, t_point begin);
+char	get_char(char **tab, t_point size, t_point begin);
 int		get_actual_begin_idx(t_point begin);
-void  flood_fill(char **tab, t_point size, t_point begin);
+void	flood_fill(char **tab, t_point size, t_point begin);
+void	define_temp(int x, int y, t_point *temp);
+void	derived_flood_fill(char **tab, t_point size, t_point begin, char replace_char);
 #endif
